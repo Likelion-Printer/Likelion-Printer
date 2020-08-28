134a3d8b4e4a708729608e0c8b627e0f88a2fa8e
Koo <mun90505@naver.com>
2020-08-28 18:48:41 +0900

    유저 DB 커스텀(주문시간, 픽업날짜, 잔액)

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin 
 
 # Register your models here.
+
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+        list_display = [
+        'order_time',
+        'pickup_time',
+        'current_balance',]
+
+        fieldsets = UserAdmin.fieldsets + (
+            ('order_info', 
+            {'fields': 
+            (        
+            'order_time',
+            'pickup_time',
+            'current_balance',)
+            }
+            ) ,
+        )
+        readonly_fields = ('order_time','current_balance' )
+        
+        add_fieldsets = UserAdmin.add_fieldsets + (
+            (None, {'fields': ('custom_field',)}),
+        )
+
--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib import auth
+import datetime
 
 # Create your models here.
 class User(AbstractUser):
-    order_time = models.DateTimeField(auto_now_add = True)
-    pickup_time = models.DateTimeField()
-    current_balance = models.DecimalField(max_digits = 15, decimal_places= 6)
+    order_time = models.DateTimeField(auto_now = True)
+    pickup_time = models.DateTimeField(auto_now = False, default = datetime.datetime.now())
+    current_balance = models.DecimalField(max_digits = 15, decimal_places= 2, default = 0)
+
+    def __str__(self):
+        return self.username
