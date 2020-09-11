24038ce48b4bffa997c419cda6ea7a72fd5675ef
Koo <mun90505@naver.com>
2020-09-12 04:13:12 +0900

    order 이름 수정

--- orders/admin.py
+++ orders/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from . import models
 
+
 # Register your models here.
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = (
+        "customer",
         "order_time",
         "pickup_time",
         "status",
