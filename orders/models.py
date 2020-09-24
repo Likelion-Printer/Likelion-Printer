c90eb9f2175053ccbd5fd26d46e13f1fbc07b2c0
jooeun <joonie811@naver.com>
2020-09-25 02:57:53 +0900

    Change view

--- orders/models.py
+++ orders/models.py
@@ -5,12 +5,14 @@ from datetime import timedelta, timezone, datetime
 
 # Create your models here.
 class Order(models.Model):
+    # 파일 추가
     is_canceled = models.BooleanField(default=False)
+    is_in_process = models.BooleanField(default=True)
     customer = models.ForeignKey(
         "accounts.User", on_delete=models.CASCADE, related_name="orders"
     )
     printer_house = models.ForeignKey(
-        "confirmations.Printer_house", on_delete=models.CASCADE, related_name="orders",
+        "confirmations.Printer_house", on_delete=models.CASCADE, related_name="orders", null=True
     )
 
     order_time = models.DateTimeField(auto_now=True)
@@ -28,6 +30,9 @@ class Order(models.Model):
     OPTION_COLOR = (("black", "흑백"), ("color", "칼라"))
     OPTION_PRINT = (("single", "단면"), ("double", "양면"))
     OPTION_PAGES = (("four", "4개"), ("two", "2개"), ("one", "1개"))
+    OPTION_DIRECTIONS = (("horizontal", "가로"), ("vertical", "세로"))
+    OPTION_FLIP = (("horizontal", "옆으로 넘김"), ("vertical", "위로 넘김"))
+
     options_color = models.CharField(
         max_length=200, choices=OPTION_COLOR, default="black"
     )
@@ -40,6 +45,16 @@ class Order(models.Model):
     number_of_pages = models.IntegerField(
         default=1, validators=[MaxValueValidator(70), MinValueValidator(1)]
     )
+    options_directions = models.CharField(
+        max_length=200, choices=OPTION_DIRECTIONS, default="vertical"
+    )
+    options_directions = models.CharField(
+        max_length=200, choices=OPTION_DIRECTIONS, default="vertical"
+    )
+    options_flip = models.CharField(
+        max_length=200, choices=OPTION_FLIP, default="horizontal"
+    )
+    comments = models.TextField()
 
     def __str__(self):
         return f"{self.customer.username}-{self.pickup_time}"
