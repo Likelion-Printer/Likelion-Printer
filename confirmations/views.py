71a8f7bd13708a6ad723c37c16e63cc44bfbb823
Koo <mun90505@naver.com>
2020-09-25 15:56:38 +0900

    confirmations에서 마이페이지에  본인 주문 확인기능 추가

--- confirmations/views.py
+++ confirmations/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from accounts import models as accounts_models
+from orders import models as orders_models
 
 # Create your views here.
 def my_order(request):
-    return render(request, 'my_order.html')
+    user_id = request.user.id
+    order_user = get_object_or_404(accounts_models.User, id=user_id)
+    myorder = order_user.orders.all()  # related name 사용
+
+    return render(
+        request, "my_order.html", {"myorder": myorder, "order_user": order_user}
+    )
