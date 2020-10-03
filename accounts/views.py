b7149582f3dd8fea43d5292651f3e52313645a98
= <mun90505@naver.com>
2020-10-04 00:27:31 +0900

    conflict 해결

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.contrib import auth
 from django.contrib.auth import get_user_model
 import requests
