36b4a0a5ea149ccc0e08aff2dded162a6303c03a
= <mun90505@naver.com>
2020-10-04 00:24:49 +0900

    오류 해결

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 from django.contrib.auth import get_user_model
+import requests
 User = get_user_model()
 # Create your views here.
 def login(request):
@@ -35,3 +36,56 @@ def logout(request):
 
 def staff_login(request):
     return render(request, 'staff_login.html')
+
+
+
+def kakao_login(request):
+    # REST_API_KEY = os.environ.get("K_KEY")
+    REST_API_KEY = "0aa937d977a340d30be56ee87e9c2f67"
+    REDIRECT_URI = "http://127.0.0.1:8000/accounts/login/kakao/callback"
+    return redirect(f"https://kauth.kakao.com/oauth/authorize?client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}&response_type=code")
+
+class KakaoException(Exception):
+    pass
+
+def kakao_callback(request):
+    try:
+        code =request.GET.get("code")
+        REST_API_KEY = "0aa937d977a340d30be56ee87e9c2f67"
+        REDIRECT_URI = "http://127.0.0.1:8000/accounts/login/kakao/callback"
+        token_request = requests.get(f"https://kauth.kakao.com?grant_type=authorization_code&client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}&code={code}")
+        token_json = token_request.json()
+        error = token_json.get("error", None)
+        if error is not None:
+            raise KakaoException()
+        
+        access_token = token_json.get("access_token")
+        profile_request = requests.get(
+            "https://kapi.kakao.com/v1/user/me",
+            headers={"Authorization": f"Bearer {access_token}"},
+        )
+        profile_json = profile_request.json()
+        email = profile_json.get("kaccount_email", None)
+        if email is None:
+            raise KakaoException()
+        properties = profile_json.get("properties")
+        nickname = properties.get("nickname")
+        profile_image = properties.get("profile_image")
+        try:
+            user = models.User.objects.get(email=email)
+            if user.login_method != models.User.LOGING_KAKAO:
+                raise KakaoException()
+        except models.User.DoesNotExist:
+            user = models.User.objects.create(
+                email=email,
+                username=email,
+                first_name=nickname,
+                login_method=models.User.LOGING_KAKAO,
+                email_verified=True,
+            )
+            user.set_unusable_password()
+            user.save()
+        login(request, user)
+        return redirect(reverse("core:home"))
+    except KakaoException:
+        return redirect(reverse("accounts:login"))
