87c504a2624d559bd140313d6ab4acf1682ddb69
문지현 <17273@naver.com>
2020-11-23 17:36:27 +0900

    지현

--- orders/static/orders/js/cancel.js
+++ orders/static/orders/js/cancel.js
@@ -1,11 +1,24 @@
-const id = document.getElementById('cancel')
-const cancelBg = document.getElementById('cancel_bg')
-const exit = document.getElementById('exit')
+const id = document.getElementById('cancel');
+const cancelBg = document.getElementById('cancel_bg');
+const exit = document.getElementById('exit');
+const cancel_modal = document.getElementById('cancel_modal')
+
 
 id.addEventListener('click', () => {
     cancelBg.style.display = 'block'
-})
+});
 
 exit.addEventListener('click', ()=> {
     cancelBg.style.display = 'none'
+
 })
+
+
+
+
+// $('html, body').css({'overflow': 'hidden', 'height': '100%'}); // 모달팝업 중 html,body의 scroll을 hidden시킴 
+// $('#cancel_modal').on('scroll touchmove mousewheel', function(event) { // 터치무브와 마우스휠 스크롤 방지    
+//     event.preventDefault();     
+//     event.stopPropagation();     
+//     return false; 
+// });
